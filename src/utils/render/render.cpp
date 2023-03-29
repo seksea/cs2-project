ac4c979764fd93970e218b47b37541c45a598bf0
hypnotic <hypnoticproject@protonmail.com>
2023-03-28 22:09:15 -0600

    big push, some bug fixes.

---
+++ src/utils/render/render.cpp
@@ -0,0 +1,5 @@
+//
+// Created by neonm on 3/28/2023.
+//
+
+#include "render.hpp"
---
+++ src/utils/render/render.hpp
@@ -0,0 +1,16 @@
+//
+// Created by neonm on 3/28/2023.
+//
+
+#ifndef CS2_PROJECT_RENDER_HPP
+#define CS2_PROJECT_RENDER_HPP
+
+
+
+class render {
+
+};
+
+
+
+#endif //CS2_PROJECT_RENDER_HPP
