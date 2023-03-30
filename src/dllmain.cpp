2b32259e4997e20e9312dba2569bbebbee329948
hypnotic <hypnoticproject@protonmail.com>
2023-03-29 22:14:10 -0600

    removed.

--- src/dllmain.cpp
+++ src/dllmain.cpp
@@ -6,20 +6,12 @@ void __stdcall cheat_thread( )
     g_interfaces.init( );
     g_schema.init( );
     g_hooks.init( );
-
-    while ( !GetAsyncKeyState( VK_END ) )
-        std::this_thread::sleep_for( std::chrono::milliseconds( 500 ) );
-
-    g_hooks.shutdown( );
-    g_console.shutdown( );
-    FreeLibraryAndExitThread( globals::dll, 0 );
 }
 
 int __stdcall DllMain( HMODULE mod, unsigned long reason, void *instance )
 {
-    globals::dll = mod;
-
-    if ( reason == DLL_PROCESS_ATTACH ) {
+    if ( reason == DLL_PROCESS_ATTACH )
+    {
         const auto h = CreateThread( nullptr, 0, LPTHREAD_START_ROUTINE( cheat_thread ), mod, 0, nullptr );
 
         if ( !h )
